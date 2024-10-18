3e3baf2c9c0e02a0c3b6c517add438bd1fa1d75f
MRWILL-available <mrwilltech2025@outlook.com>
2024-10-18 11:43:16 +0000

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ global.video = "";
 global.port = process.env.PORT;
 global.appUrl = process.env.APP_URL || "";
 global.email = "davidcyril209@gmail.com";
-global.location = "Abuja, Nigeria";
+global.location = "Accra, Ghana";
 global.mongodb = process.env.MONGODB_URI || "";
 global.allowJids = process.env.ALLOW_JID || "null";
 global.blockJids = process.env.BLOCK_JID || "null";
@@ -18,11 +18,11 @@ global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaeRru3ADTOE
 global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaeRru3ADTOEKPCPom0L";
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://telegra.ph/file/17c8ba84a7761eed633f6.jpg";
 global.devs = "https://t.me/deecee_x";
-global.sudo = process.env.SUDO || "";
-global.owner = process.env.OWNER_NUMBER || "";
+global.sudo = process.env.SUDO || "available";
+global.owner = process.env.OWNER_NUMBER || "233247923525";
 global.style = process.env.STYLE || "3";
-global.gdbye = process.env.GOODBYE || "false";
-global.wlcm = process.env.WELCOME || "false";
+global.gdbye = process.env.GOODBYE || "true";
+global.wlcm = process.env.WELCOME || "true";
 global.warncount = process.env.WARN_COUNT || 3;
 global.disablepm = process.env.DISABLE_PM || "false";
 global.disablegroup = process.env.DISABLE_GROUPS || "false",
@@ -32,16 +32,16 @@ global.waPresence = process.env.WAPRESENCE || "available";
 global.readcmds = process.env.READ_COMMAND || "false";
 global.readmessage = process.env.READ_MESSAGE || "false";
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "";
-global.read_status = process.env.AUTO_READ_STATUS || "true";
+global.read_status = process.env.AUTO_READ_STATUS || "flase";
 global.save_status = process.env.AUTO_SAVE_STATUS || "false";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "";
-global.read_status_from = process.env.READ_STATUS_FROM || "";
+global.read_status_from = process.env.READ_STATUS_FROM || "false";
 
 global.api_smd = "https://api-smd-1.vercel.app";
 global.scan = "https://secret-garden-43998-4daad95d4561.herokuapp.com/";
 
 global.SESSION_ID =
-  process.env.SESSION_ID ||
+  process.env.SESSION_ID |eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTUhnbGlKTzZtS1lYWHdrSGY2ZVg0ZVlkR1FqcGJqYlczOUZEaEZSZUVFaz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiMm9sVzg0NXJFTkgwMGU0b3lFNkRnN3ZhMEd3NTJYRkMzMnFsdjczcmxYbz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiIrTTFFTEtTSHh3bitPTzdCZHdERkNiRXNxK3o0QnE4NHpWM2Y2Vzc2Vkd3PSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJ5bmVnQ0hIM2w4VGFnTnc4ZGFPWlNYNmV4ZGdlVnhVdzhzV0ROd05YeGpzPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Ik9LMHVFMXVmUC81SDc1YlpmaGVDVFBKRjlseDJkYmwvanlBMk1uak01WGs9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6Indac0FkQTMrY3B1amI5WHBnbGFkRmNzTmlKQ3J3OGhxdkd2WjllV1dWRXc9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiT0ZjeUEzbjB3d3piNDhLclN3NmFUd1FYb1V4SEphYm1sdDRLVXdkYzQyaz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoicmRraiswOU16R2J5ejVZcnB3NWRlR2hJa3hUQXQ1amlubCszR0N3aUtsST0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IkJrWG5xTk9UbGhUOEhBYzdQRlNDVkhWUStIVlA3T08xMkM3V2lsQzZKcUZpMW1HMnlxT204TEpnWm5BRE9TVWl1ZXpOK1U0cEZEb1NlU1psV3RWY2lBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6NTMsImFkdlNlY3JldEtleSI6Ikp5SldUT2NSc3Q0UVM1TkRtU3FlenFCdzlpVFJUYjNVRURyOHNPYkMyK2c9IiwicHJvY2Vzc2VkSGlzdG9yeU1lc3NhZ2VzIjpbXSwibmV4dFByZUtleUlkIjozMSwiZmlyc3RVbnVwbG9hZGVkUHJlS2V5SWQiOjMxLCJhY2NvdW50U3luY0NvdW50ZXIiOjAsImFjY291bnRTZXR0aW5ncyI6eyJ1bmFyY2hpdmVDaGF0cyI6ZmFsc2V9LCJkZXZpY2VJZCI6InV4VDNOUTZzUUVlWTh2Q3Uyb1RzZFEiLCJwaG9uZUlkIjoiMjFlZWM0YzQtMmViZS00MjZhLTg2ODMtOGJiMzgzNGI3YmE2IiwiaWRlbnRpdHlJZCI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImdJa0N0Z01RYmZnWm93VXJobFBia0l5TVhKbz0ifSwicmVnaXN0ZXJlZCI6dHJ1ZSwiYmFja3VwVG9rZW4iOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJZTVNaaEdid2MySUZYQjZ0RURGZDA0M2pZeFk9In0sInJlZ2lzdHJhdGlvbiI6e30sInBhaXJpbmdDb2RlIjoiVzg3Vlk2MUciLCJtZSI6eyJpZCI6IjIzMzI0NzkyMzUyNTo3N0BzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDSkROazdzSEVPV1B5YmdHR0FFZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiQmJvb0pqVVlJczBWQTMzYVlVbUtvZUlpd1FPNHErSU1aS2phY1hVSFFTbz0iLCJhY2NvdW50U2lnbmF0dXJlIjoiM3A3cTByREpYNE5uaEVIbWVpMWo1YWRiTUw4OWp6UkVScUlMUU8zS1hVd0R0UjMwVStVZGlDV1ZZREtOUDd6RndzU0J0a0Yrc1h6YmxzbUtublBWQnc9PSIsImRldmljZVNpZ25hdHVyZSI6InNpZkFISlRKenZSYXdmbzQrVUZlUEt2R09QOFFtcVY4MEdIRXAwRS9CRDEzMllkaEJ3a0tid0NwTStCVkZhN2JaN2xmcTJEY25GdSs0Q1dyd0FQMWhRPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjMzMjQ3OTIzNTI1Ojc3QHMud2hhdHNhcHAubmV0IiwiZGV2aWNlSWQiOjB9LCJpZGVudGlmaWVyS2V5Ijp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiQlFXNktDWTFHQ0xORlFOOTJtRkppcUhpSXNFRHVLdmlER1NvMm5GMUIwRXEifX1dLCJwbGF0Zm9ybSI6ImFuZHJvaWQiLCJsYXN0QWNjb3VudFN5bmNUaW1lc3RhbXAiOjE3MjkyNTEzMTQsIm15QXBwU3RhdGVLZXlJZCI6IkFBQUFBTGRPIn0=|
   ""
 module.exports = {
   menu: process.env.MENU || "2",
@@ -52,7 +52,7 @@ module.exports = {
   author: process.env.PACK_AUTHER || "QUEEN_ANITA-V2",
   packname: process.env.PACK_NAME || "A N I T A",
   botname: process.env.BOT_NAME || "QUEEN_ANITA-V2",
-  ownername: process.env.OWNER_NAME || "David Cyril",
+  ownername: process.env.OWNER_NAME || "MRWILL-available",
   errorChat: process.env.ERROR_CHAT || "",
   KOYEB_API: process.env.KOYEB_API || "false",
   REMOVE_BG_KEY: process.env.REMOVE_BG_KEY || "",
